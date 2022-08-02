ac3a1b87c08f5072dccaa5b689312834367d6e43
imrancs058 <imran.afzalrnssol.com>
2022-08-02 11:00:49 +0500

    Used axios to call API Form and table tags to display the data, useEffect and useState hooks

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ function App() {
       price: parseInt(e.target[2].value),
       stock: parseInt(e.target[3].value)
     }
-    const response = await axios.post('http://localhost:2022/book', book);
+    await axios.post('http://localhost:2022/book', book);
 
     const bookList = await axios.get('http://localhost:2022/book');
     setBooks(bookList.data);
-
-    console.log(response);
   }
 
+  // useState is a hook that allows us to create a state variable in our component and also update it when we want to. It takes a single argument, the initial value of the state variable. We can then use this state variable to update the component. We can also use this state variable to get the current value of the state variable. 
   const [books, setBooks] = React.useState([]);
 
+  //  useEffect is a hook that runs after the component is rendered, but before the component is updated. 
   React.useEffect(() => {
     axios.get('http://localhost:2022/book')
       .then(response => {
@@ -35,6 +35,13 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Book Store</h1>
+      {/* This is a form, use are using <form> tag, onSubmit is a problem, that takes a function. 
+      
+      I created a function above, and assigned to it. 
+      Up there I am getting data from form
+      to Call APIs I am using axios.post, and putting the data in the body.
+       */}
       <form onSubmit={onSubmit}>
         <label>Book Name:</label>
         <input type="text" name="bookName" />
@@ -46,7 +53,14 @@ function App() {
         <input type="text" name="stock" />
         <button type="submit">Save</button>
       </form>
+{/* This is table, simple as we create in HTML
 
+Here I am using books.map. If you are familiar with Array.map function, you can understand this.
+If you are not, try to google it.
+
+Here I am using books.map to generate rows dynamically.
+
+ */}
       <table>
         <thead>
           <tr>
@@ -57,12 +71,6 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {/* <tr>
-            <td>PF</td>
-            <td>Unknown</td>
-            <td>12</td>
-            <td>12</td>
-          </tr> */}
           {books.map(book => (
             <tr>
               <td>{book.name}</td>
