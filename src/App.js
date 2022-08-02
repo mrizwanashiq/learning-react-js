0f086889f1e4189242b61a626172beb642e0ac06
imrancs058 <imran.afzalrnssol.com>
2022-08-02 10:54:32 +0500

    Used axios to call API Form and table tags to display the data, useEffect and useState hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
 function App() {
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    console.log('submit');
+    const book = {
+      name: e.target[0].value,
+      author: e.target[1].value,
+      price: parseInt(e.target[2].value),
+      stock: parseInt(e.target[3].value)
+    }
+    const response = await axios.post('http://localhost:2022/book', book);
+
+    const bookList = await axios.get('http://localhost:2022/book');
+    setBooks(bookList.data);
+
+    console.log(response);
+  }
+
+  const [books, setBooks] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:2022/book')
+      .then(response => {
+        setBooks(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  , []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <label>Book Name:</label>
+        <input type="text" name="bookName" />
+        <label>Author Name:</label>
+        <input type="text" name="authorName" />
+        <label>Price:</label>
+        <input type="text" name="price" />
+        <label>Stock:</label>
+        <input type="text" name="stock" />
+        <button type="submit">Save</button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Book Name</th>
+            <th>Author Name</th>
+            <th>Price</th>
+            <th>Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr>
+            <td>PF</td>
+            <td>Unknown</td>
+            <td>12</td>
+            <td>12</td>
+          </tr> */}
+          {books.map(book => (
+            <tr>
+              <td>{book.name}</td>
+              <td>{book.author}</td>
+              <td>{book.price}</td>
+              <td>{book.stock}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
