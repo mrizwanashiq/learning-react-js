89eb62cac90f8d12268d97021bd8d22f60827fa3
mrizwanashiq <mrizwanashiq@outlook.com>
2022-08-17 11:37:02 +0500

    Removed redux code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import store from './redux/store';
-import { Provider } from 'react-redux'
 import Counter from './pages/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Counter />
-    </Provider>
+    <Counter />
   </React.StrictMode>
 );
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './app'
-
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-})
