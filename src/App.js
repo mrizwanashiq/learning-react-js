3a8e9c8a3bb8a3778212210274d31300f1dabaf0
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2023-03-21 21:31:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // And import pages
-
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Home from "./pages/home";
 
-// Now let's move to routing.
-
+// Now let's move to routing part.
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} /> // Here we are saying that if the
-				path is `/` then display the `Home` component.
-				<Route path="/login" element={<Login />} /> // Here we are saying that
-				if the path is `/login` then display the `Login` component.
-				<Route path="/register" element={<Register />} /> // Here we are saying
-				that if the path is `/register` then display the `Register` component.
+				<Route path="/" element={<Home />} />
+					{/* Here we are saying that if the path is `/` then display the `Home` component.*/}
+				<Route path="/login" element={<Login />} />
+					{/* Here we are saying that if the path is `/login` then display the `Login` component. */}
+				<Route path="/register" element={<Register />} />
+					{/* Here we are saying that if the path is `/register` then display the `Register` component. */}
 			</Routes>
 		</BrowserRouter>
 	);
